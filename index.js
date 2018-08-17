331c58eaa51de9aab96dd4cf29a783f36de41c8e
Dom Barker <dom.barker808@gmail.com>
2018-08-17 11:52:11 +0100

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "jest/valid-expect": [1],
+    "react/display-name": [2],
+    "no-undef": [ 2 ],
+    "react/prop-types": [1],
+    "eqeqeq": [2, "always"],
+    "quotes": [
+      1,
+      "single",
+      { "avoidEscape": true }
+  ],
+  "semi": [
+      1,
+      "never"
+  ]
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:jest/recommended"],
+  "plugins": [ "react", "jest" ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+        "jsx": true,
+        "experimentalObjectRestSpread": true
+    }
+  }
+}
